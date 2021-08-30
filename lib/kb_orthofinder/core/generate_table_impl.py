b43e15c6c28678eb89b967880a0c51d2bcddc0a6
Sam Seaver <samseaver@gmail.com>
2021-08-30 17:45:43 -0500

    Reorganizing, formatting and coloring of table entries

--- lib/kb_orthofinder/core/generate_table_impl.py
+++ lib/kb_orthofinder/core/generate_table_impl.py
@@ -28,6 +28,19 @@ class GenerateTableImpl:
 
         html_lines.append("<tbody>")
 
+        # Collecting hits that come from the same family
+        # regardless of function
+        family_hit_dict=dict()
+        for function in functions:
+            for family in functions[function]:
+
+                if(family not in family_hit_dict):
+                    family_hit_dict[family]=list()
+
+                for hit in functions[function][family]['hits']:
+                    family_hit_dict[family].append(hit['feature'])
+
+
         ############################################################################
         # Each row in the table is unique to the combination of enzymatic annotation
         # curated Arabidopsis enzyme, for which we show the retrieved predicted ortholog
@@ -39,26 +52,42 @@ class GenerateTableImpl:
             for family in functions[function]:
                 for ortholog in sorted(functions[function][family]['orthologs']):
 
-                    hit_dict=dict()
                     hit_list=list()
                     for hit in functions[function][family]['hits']:
                         hit_list.append(hit['feature'])
-                        hit_dict[hit['feature']]=hit['seqid']
 
                     cls_list=list()
                     for cls_ftr in functions[function][family]['cluster'].keys():
                         for cls_olg in functions[function][family]['cluster'][cls_ftr]:
                             if(cls_olg == ortholog):
                                 seqid=functions[function][family]['cluster'][cls_ftr][cls_olg]
+                                
+                                # reformat seqid to two decimal points
+                                seqid = "{0:.2f}".format(float(seqid))
+
                                 ftr_seqid_str = cls_ftr+":"+seqid
-                                if(cls_ftr in hit_dict):
+                                if(cls_ftr in hit_list):
+                                    # color green
+                                    ftr_seqid_str = "<font color=\"red\">"+ftr_seqid_str+"</font>"
+                                elif(cls_ftr in family_hit_dict[family]):
+                                     # color red
+                                     ftr_seqid_str = "<font color=\"light blue\">"+ftr_seqid_str+"</font>"
+                                else:
+                                    # default color
+                                    pass
+
+                                if(cls_ftr in family_hit_dict):
                                     ftr_seqid_str = "<b>"+ftr_seqid_str+"</b>"
-                                cls_list.append(ftr_seqid_str)
+                                cls_list.append([float(seqid),ftr_seqid_str])
+
+                    cls_sorted_list = list()
+                    for entry in sorted(cls_list, key = lambda x: x[0], reverse=True):
+                        cls_sorted_list.append(entry[1])
 
                     html_lines.append("<tr>")
                     internal_row_line = "</td><td>".join([function,ortholog,
                                                           ", ".join(sorted(hit_list)),
-                                                          ", ".join(sorted(cls_list))])
+                                                          ", ".join(cls_sorted_list)])
                     html_lines.append("<td>"+internal_row_line+"</td>")
                     html_lines.append("</tr>")
 
