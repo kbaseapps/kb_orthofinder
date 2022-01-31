a04d72b90ebc910cfcb0df45de72c5d88e129034
Sam Seaver <samseaver@gmail.com>
2022-01-31 15:37:29 -0600

    Updated location of ref data and test data

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -12,20 +12,25 @@ if [ $# -eq 0 ] ; then
   sh ./scripts/start_server.sh
 elif [ "${1}" = "test" ] ; then
   echo "Initializing test data"
-  TEST_DATA=$(grep test_data ./deploy.cfg | awk '{print $3}')
-  cd /kb/module/data
-  if [ -f ${TEST_DATA}/SpeciesIDs.txt ] ; then
+  TEST_DATA=$(grep ^test_data ./deploy.cfg | awk '{print $3}')
+  mkdir -p /kb/module/work/tmp/test_data
+  cd /kb/module/work/tmp/test_data
+  if [ -d ${TEST_DATA} ] && [ -f ${TEST_DATA}/SpeciesIDs.txt ] ; then
       echo "Test data already present, skipping initialization"
   else
-    PROTEIN_FAMILY_FILE="${TEST_DATA}.tar.gz"
-    wget http://bioseed.mcs.anl.gov/~seaver/Files/PlantSEED/${PROTEIN_FAMILY_FILE}
-    tar -zxf $PROTEIN_FAMILY_FILE
-    if [ -f ${TEST_DATA}/SpeciesIDs.txt ] ; then
-      echo "Test data initialized"
-    else
-      echo "Test data initialization failed"
-      exit 1
-    fi
+      PROTEIN_FAMILY_FILE="${TEST_DATA}.tar.gz"
+      if ! [ -f ${PROTEIN_FAMILY_FILE} ] ; then
+	  echo "Fetching ${PROTEIN_FAMILY_FILE}"
+	  wget https://web.cels.anl.gov/~seaver/KBase_App_Files/${PROTEIN_FAMILY_FILE}
+      fi
+      echo "Gunzipping ${PROTEIN_FAMILY_FILE}"
+      tar -zxf $PROTEIN_FAMILY_FILE
+      if [ -f ${TEST_DATA}/SpeciesIDs.txt ] ; then
+	  echo "Test data initialized"
+      else
+	  echo "Test data initialization failed"
+	  exit 1
+      fi
   fi
   echo "Running Tests"
   cd /kb/module
@@ -36,7 +41,7 @@ elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
   cd /data
   PROTEIN_FAMILY_FILE="OrthoFinder_Phytozome_Reference.tar.gz"
-  wget http://bioseed.mcs.anl.gov/~seaver/Files/PlantSEED/${PROTEIN_FAMILY_FILE}
+  wget https://web.cels.anl.gov/~seaver/KBase_App_Files/${PROTEIN_FAMILY_FILE}
   echo "Gunzipping file"
   tar -zxf $PROTEIN_FAMILY_FILE
   if [ -f OrthoFinder_Phytozome_Reference/SpeciesIDs.txt ] ; then
