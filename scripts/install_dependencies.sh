adb9eb3b6d2c5582e21e075b1030821e08a76506
Sam Seaver <samseaver@gmail.com>
2023-11-14 09:29:55 -0600

    Merging commands into a single line

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -10,16 +10,13 @@ wget --quiet https://github.com/davidemms/OrthoFinder/releases/download/2.5.2/Or
 tar -zxf OrthoFinder_source.tar.gz
 mv OrthoFinder_source /usr/bin/orthofinder
 
-# OrthoFinder dependencies
-DEBIAN_FRONTEND=noninteractive apt-get install -y mcl
-apt-get install -y mafft
-apt-get install -y fasttree
+# OrthoFinder dependencies and grace for report generation
+DEBIAN_FRONTEND=noninteractive apt-get install -y mcl grace mafft fasttree
 wget --quiet https://github.com/bbuchfink/diamond/releases/download/v2.0.9/diamond-linux64.tar.gz
 tar -zxf diamond-linux64.tar.gz diamond
 mv diamond /usr/bin/diamond
 
 # Dependcies for generating plot for main report
-DEBIAN_FRONTEND=noninteractive apt-get install -y grace
 # Using a fork with fixes for Python 3
 git clone https://github.com/samseaver/pygrace.git
 cd pygrace
@@ -31,6 +28,4 @@ git clone -b kbase_release https://github.com/ModelSEED/PlantSEED /kb/module/Pla
 # Dependencies for Bokeh JS Plots
 conda install -c conda-forge phantomjs
 pip install --upgrade pip
-pip install scipy
-pip install bokeh
-pip install selenium
+pip install scipy bokeh selenium
